6c7a0572f748a9e39905293b742643302b6830bc
J. Gregg Thomason <gregg@neurobashing.com>
2012-02-18 12:06:24 -0500

    check for empty dirs; obviously a submodule isn't active. (obviously?)

--- update.sh
+++ update.sh
@@ -5,3 +5,5 @@ git submodule update
 git submodule foreach git pull origin master
 cd bundle/snipmate-snippets
 git pull
+echo "checking for empty bundle dirs; you need to run git submodule init"
+find bundle -type d -empty \! -path "*/.git/*"
