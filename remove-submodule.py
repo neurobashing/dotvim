7ecd325b1cad513e02e53cbb003d1a586b8c6106
J. Gregg Thomason <gregg.thomason@asti-usa.com>
2012-03-28 12:09:38 -0400

    removed module TaskList

--- remove-submodule.py
+++ remove-submodule.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 
-import sys,subprocess
+import sys
+import subprocess
+import shutil
 
 if len(sys.argv) < 2:
   exit('Please enter a module name, ie what is under bundles.')
 else:
   modulename = sys.argv[1]
+
+shutil.rmtree('bundle/'+modulename)
 
 subprocess.call(["git","rm","-rf","bundle/"+modulename])
 subprocess.call(['git','config','--remove-section',"submodule.bundle/"+modulename])
