b719a0948ba1b25c7e920fa9ff2e25fb11fbf0f7
J. Gregg Thomason <gregg@neurobashing.com>
2012-10-13 10:15:26 -0400

    just init submodule.

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo "remember to run 'git submodule init' if you've added stuff recently."
-git pull
+git pull origin master
+git submodule init
 git submodule update
 git submodule foreach git pull origin master
 cd bundle/snipmate-snippets
